8cd2e267abe48857f4cf01090f635893a9071f2c
jw00541 <wightjjohn@gmail.com>
2026-05-27 17:46:25 -0400

    5272026: placeholder, reading about Consumer<T> behavior

--- src/main/java/frc/robot/subsystems/shooter/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelConstants.java
@@ -2,6 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/** Rewrite from scratch for practice only. JW */
 package frc.robot.subsystems.shooter;
 
 import static edu.wpi.first.units.Units.Inches;
