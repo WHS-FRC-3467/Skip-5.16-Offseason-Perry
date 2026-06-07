c5f7789c536b4b20718f1e44c0ea876780d38075
Driver3467 <frc_3467_driver@team3467.org>
2026-06-07 10:34:03 -0400

    Update at 'Sun Jun 07 10:34:03 EDT 2026'

--- src/main/java/frc/robot/generated/ChoreoTraj.java
+++ src/main/java/frc/robot/generated/ChoreoTraj.java
@@ -24,7 +24,7 @@ public record ChoreoTraj(
     public static final ChoreoTraj AggresiveAuto1 = new ChoreoTraj(
         "AggresiveAuto1",
         OptionalInt.empty(),
-        6.35638,
+        5.47287,
         new Pose2d(4.45292, 7.45197, Rotation2d.fromRadians(1.5708)),
         new Pose2d(2.19643, 5.40836, Rotation2d.fromRadians(-0.68187))
     );
