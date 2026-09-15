117173b9a3a57f92b855fa5d290c8035b4f327b7
CJendantix <75325223+CJendantix@users.noreply.github.com>
2026-09-15 18:37:49 -0400

    Revert "Revert Battlecry-Auto Specific Changes"

--- src/main/java/frc/robot/commands/autos/BAutoUnsafe.java
+++ src/main/java/frc/robot/commands/autos/BAutoUnsafe.java
@@ -44,7 +44,10 @@ public class BAutoUnsafe {
 
     public static Optional<AutoOption> create(AutoContext ctx, boolean shouldMirror) {
         List<String> names =
-                List.of(ChoreoTraj.B1Unsafe.name(), ChoreoTraj.B2.name(), ChoreoTraj.B3.name());
+                List.of(
+                        ChoreoTraj.B1Unsafe.name(),
+                        ChoreoTraj.B2.name(),
+                        ChoreoTraj.Handoff.name());
 
         List<Trajectory<SwerveSample>> trajectories =
                 AutoUtil.loadTrajectories(names, shouldMirror).orElse(null);
@@ -106,6 +109,9 @@ public class BAutoUnsafe {
                                                             .setHoodAngle(Degrees.of(0.0))
                                                             .asProxy(),
                                                     thirdFollow.asProxy()));
+
+                            routine.observe(thirdFollow.done())
+                                    .onTrue(AutoCommands.fullSend(ctx, shouldMirror));
 
                             return routine;
                         }));
